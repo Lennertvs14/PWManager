30625028d0c380a06d88efc7a387be82edb14a54
LennertvS <lennertvansplunder@gmail.com>
2022-12-12 19:42:08 +0100

    Created mock data and a base unit test class.

--- API/API.Test/GetTest.cs
+++ API/API.Test/GetTest.cs
@@ -1,24 +1,110 @@
-﻿namespace API.Test
+﻿using Microsoft.AspNetCore.Http;
+
+namespace API.Test
 {
     [TestClass]
     public class GetTest : UnitTest
     {
         /// <summary>
-        /// Get method test
+        /// As an admin with corresponding authorisation I must be able to receive a List of all users.
         /// </summary>
         [TestMethod]
-        public void TestCase()
+        public void GetAllUsersWithAuthorisation()
         {
             // Arrange
-            var currentUsers = GetData("User");
+            var currentUsers = GetData<User>("User");
 
             // Apply
-            var action = Controller.Get();
+            var action = Controller.GetUsers();
             var actionResult = action.ToString();
 
             // Assert
             Assert.AreEqual(MockDatabase.Users.Count(), currentUsers.Count);
+            Assert.AreEqual(okObjectResult, actionResult);
+        }
+
+        /// <summary>
+        /// As a user I must be able to log in when submitting correct account credentials.
+        /// </summary>
+        [TestMethod]
+        public void LogOnWithValidAccount()
+        {
+            // Arrange
+            var user = GetRandomExistingUser();
+
+            // Apply
+            var action = Controller.LogOn(user);
+            var actionResult = action.ToString();
+
+            // Assert
             Assert.AreEqual(okResult, actionResult);
+        }
+
+        private User GetRandomExistingUser()
+        {
+            var users = MockDatabase.Users;
+            var id = random.Next(1, users.Count());
+            var user = users
+                .Where(u => u.Id == id)
+                .Select(u => u)
+                .Single();
+            return user;
+        }
+
+        [TestMethod]
+        public void LogOnWithWrongAccount()
+        {
+            // Arrange
+            var user = GetNewUserObject("INVALID", "INVALID");
+
+            // Apply
+            var action = Controller.LogOn(user);
+            var actionResult = action.ToString();
+
+            // Assert
+            Assert.AreEqual(badRequestResult, actionResult);
+        }
+
+        private User GetNewUserObject(string username, string password)
+        {
+            var user = GetRandomExistingUser();
+            user.Id = GetNewId(typeof(User));
+            user.UserName = username;
+            user.Password = password;
+            return user;
+        }
+
+        [TestMethod]
+        public void GetPasswordWithNoLength()
+        {
+            // Arrange
+            int defaultLength = 12;
+
+            // Act
+            var password = UserRepository.GetGeneratedPassword();
+            var action = Controller.GetPassword(0);
+            var actionResult = action.ToString();
+    
+            // Assert
+            Assert.AreEqual(okObjectResult, actionResult);
+            Assert.AreEqual(defaultLength, password.Length);
+        }
+
+        [TestMethod]
+        public void GetPasswordWithInvalidLength()
+        {
+            // Arrange
+            int invalidLength = -10;
+            int defaultLength = 12;
+
+            // Act
+            var password = UserRepository.GetGeneratedPassword(invalidLength);
+            var action = Controller.GetPassword(invalidLength);
+            var actionResult = action.ToString();
+
+            // Assert
+            Assert.AreEqual(okObjectResult, actionResult);
+            Assert.AreEqual(defaultLength, password.Length);
         }
     }
 }
--- API/API.Test/UnitTest.cs
+++ API/API.Test/UnitTest.cs
@@ -1,9 +1,12 @@
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+
 namespace API.Test
 {
     public class UnitTest
     {
         private DbContextOptions<DatabaseContext> options;
         protected DatabaseContext MockDatabase;
+        protected UserRepository UserRepository;
         protected UserController Controller;
 
         public UnitTest()
@@ -13,30 +16,55 @@ namespace API.Test
                 .Options;
             MockDatabase = new DatabaseContext(options);
             FillTheMockDatabase();
-            Controller = new UserController(MockDatabase);
+            UserRepository = new UserRepository(MockDatabase);
+            Controller = new UserController(UserRepository);
         }
 
-        protected static readonly string okResult = typeof(OkObjectResult).FullName!;
-        protected static readonly string badResult = typeof(BadRequestObjectResult).FullName!;
+        protected static readonly string okObjectResult = typeof(OkObjectResult).FullName!;
+        protected static readonly string okResult = typeof(OkResult).FullName!;
+        protected static readonly string badRequestResult = typeof(BadRequestObjectResult).FullName!;
+        public readonly Random random = new Random();
 
         protected void FillTheMockDatabase()
         {
             bool databaseIsFilled = MockDatabase.Users.Any();
             if (!databaseIsFilled)
             {
-                var users = GetData("User");
+                var users = GetData<User>("User");
                 foreach (var user in users)
                 {
-                    MockDatabase.Users.Add((User)user);
+                    MockDatabase.Users.Add(user);
                 }
+                var roles = GetData<Role>("Role");
+                foreach(var role in roles)
+                {
+                    MockDatabase.Roles.Add(role);
+                }
+                var privileges = GetData<Privilege>("Privilege");
+                foreach (var privilege in privileges)
+                {
+                    MockDatabase.Privileges.Add(privilege);
+                }
+                MockDatabase.SaveChanges();
             }
-            MockDatabase.SaveChanges();
         }
 
-        protected List<User> GetData(string fileName)
+        protected List<T> GetData<T>(string fileName)
         {
             string path = @"MockData\" + fileName + ".json";
-            return JsonConvert.DeserializeObject<List<User>>(File.ReadAllText(path));
+            return JsonConvert.DeserializeObject<List<T>>(File.ReadAllText(path));
+        }
+
+        protected int GetNewId(Type type)
+        {
+            var objects = GetData<User>(type.Name.ToString());
+            var identities = objects.Select(o => o.Id);
+            var id = identities.Last();
+            while (identities.Contains(id))
+            {
+                id++;
+            }
+            return id;
         }
     }
 }
