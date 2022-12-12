4cd8262e647d65c73d7ab3d3369b2621fdcae701
LennertvS <lennertvansplunder@gmail.com>
2022-12-12 19:33:23 +0100

    Added controller methods to log in, get a generated password, and to log on.

--- API/API/Controllers/UserController.cs
+++ API/API/Controllers/UserController.cs
@@ -4,17 +4,52 @@ namespace API.Controllers
     [Route("[controller]")]
     public class UserController : ControllerBase
     {
-        private readonly DatabaseContext _databaseContext; 
+        private readonly IUserRepository _userRepository;
 
-        public UserController(DatabaseContext database)
+        public UserController(IUserRepository userRepository)
         {
-            _databaseContext = database;
+            _userRepository = userRepository;
         }
 
         [HttpGet]
-        public ActionResult Get()
+        public ActionResult GetUsers()
         {
-            return Ok( _databaseContext.Users );
+            // TODO: Get a user's identity.
+            int userId = 1;
+            try
+            {
+                var users = _userRepository.GetAllUsers(userId);
+                return Ok(users);
+            }
+            catch
+            {
+                return BadRequest("[FAILED] You are not authorised.");
+            }
+        }
+        public ActionResult GetPassword(int length)
+        {
+            try
+            {
+                var password = _userRepository.GetGeneratedPassword(length);
+                return Ok(password);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest($"[ERROR] {ex.Message}");
+            }
+        }
+
+        public ActionResult LogOn(User user)
+        {
+            try
+            {
+                var getUser = _userRepository.LogOn(user);
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest("[FAILED] Wrong credentials.");
+            }
         }
     }
 }
